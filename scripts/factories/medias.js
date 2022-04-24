685ed49d73824d90e5e454cc1213e8e7ee2996c1
8UK0W5K1 <jeremie.deldem@gmail.com>
2022-04-24 21:45:10 +0200

    now also handle video medias

--- scripts/factories/medias.js
+++ scripts/factories/medias.js
@@ -1,11 +1,11 @@
 function mediasFactory(data) {
-  const { id, photographerId, title, image, likes, date, price } = data;
+  const { id, photographerId, title, image, video, likes, date, price } = data;
 
   let srcMedia = `assets/images/photographers/${photographerId}/`;
   if (image) {
     srcMedia += image;
   } else {
-    // srcMedia += video;
+    srcMedia += video;
   }
 
   function getMediasDOM() {
@@ -18,16 +18,26 @@ function mediasFactory(data) {
     mediaTitle.classList.add('media_title');
     mediaTitle.textContent = title;
 
-    /** MEDIAS => vidéo ou image */
-    const media = document.createElement('img');
-    media.classList.add('media');
-    media.setAttribute('src', srcMedia);
-    media.setAttribute('data-mediaid', id);
-    media.setAttribute('alt', title + ', closeup view');
-    media.setAttribute('role', 'link');
-    media.setAttribute('tabindex', 0);
-    figure.appendChild(media);
-
+    if (image) {
+      const media = document.createElement('img');
+      media.classList.add('media');
+      media.setAttribute('src', srcMedia);
+      media.setAttribute('data-mediaid', id);
+      media.setAttribute('alt', title + ', closeup view');
+      media.setAttribute('role', 'link');
+      media.setAttribute('tabindex', 0);
+      figure.appendChild(media);
+    } else {
+      const video = document.createElement('video');
+      video.classList.add('media');
+      video.setAttribute('type', 'video/mp4');
+      video.setAttribute('src', srcMedia);
+      video.setAttribute('data-mediaid', id);
+      video.setAttribute('alt', title + ', closeup view');
+      video.setAttribute('role', 'link');
+      video.setAttribute('tabindex', 0);
+      figure.appendChild(video);
+    }
     figure.appendChild(figcaption);
     figcaption.appendChild(mediaTitle);
     return figure;
@@ -38,6 +48,7 @@ function mediasFactory(data) {
     photographerId,
     title,
     image,
+    video,
     likes,
     date,
     price,
