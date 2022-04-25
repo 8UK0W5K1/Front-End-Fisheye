d5cfeb7fdf075ce2820b22fe12ab58110ddf2eb7
8UK0W5K1 <jeremie.deldem@gmail.com>
2022-04-25 13:00:12 +0200

    stuck with this gallery but keep on tryin

--- scripts/factories/medias.js
+++ scripts/factories/medias.js
@@ -18,8 +18,9 @@ function mediasFactory(data) {
     mediaTitle.classList.add('media_title');
     mediaTitle.textContent = title;
 
-    const link = document.createElement('a');
-    figure.appendChild(link);
+    const links = document.createElement('a');
+    links.setAttribute('onclick', 'event.preventDefault()');
+    figure.appendChild(links);
 
     if (image) {
       const media = document.createElement('img');
@@ -29,11 +30,12 @@ function mediasFactory(data) {
       media.setAttribute('alt', title + ', closeup view');
       media.setAttribute('role', 'link');
       // media.setAttribute('href', `${media.src}`);
-      console.log(media.src);
+
+      // console.log(media.src);
       media.setAttribute('tabindex', 0);
 
-      link.appendChild(media);
-      link.setAttribute('href', `${media.src}`);
+      links.appendChild(media);
+      links.setAttribute('href', `${media.src}`);
     } else {
       const video = document.createElement('video');
       video.classList.add('media');
@@ -45,8 +47,8 @@ function mediasFactory(data) {
       video.setAttribute('tabindex', 0);
       figure.appendChild(video);
 
-      link.appendChild(video);
-      link.setAttribute('href', `${video.src}`);
+      links.appendChild(video);
+      links.setAttribute('href', `${video.src}`);
     }
     figure.appendChild(figcaption);
     figcaption.appendChild(mediaTitle);
@@ -59,6 +61,44 @@ function mediasFactory(data) {
       document.querySelectorAll('a[href$=".jpg"], a[href$=".mp4"]')
     );
     console.log(links);
+
+    function displayLightbox() {
+      const gallery = document.querySelector('#photographer_gallery');
+      const lightbox = document.createElement('div');
+      lightbox.classList.add('lightbox');
+      gallery.appendChild(lightbox);
+
+      const closeImg = document.createElement('img');
+      closeImg.setAttribute('src', 'assets/icons/close.svg');
+      closeImg.setAttribute('aria-label', 'Close gallery');
+      closeImg.setAttribute('alt', 'close gallery');
+      closeImg.classList.add('close_gallery');
+
+      lightbox.appendChild(closeImg);
+
+      closeImg.addEventListener('click', closeLightbox);
+      // const closeGallery = document.querySelector('#close_gallery');
+      document.querySelector('.close_gallery').addEventListener('click', () => {
+        document.querySelector('.lightbox').style.zIndex = '1';
+      });
+    }
+
+    function closeLightbox() {
+      const gallery = document.querySelector('#photographer_gallery');
+      const lightbox = document.querySelector('.lightbox');
+      console.log('ou ca bloque ???');
+      gallery.removeChild(lightbox);
+    }
+    // const closeGallery = document.querySelector('.lightbox');
+    // closeGallery.addEventListener('click', closeLightbox);
+
+    // const closeGallery = document.querySelector('#close_gallery');
+    // closeGallery.addEventListener('click', closeLightbox);
+
+    links.forEach((link) => {
+      console.log(link);
+      link.addEventListener('click', displayLightbox);
+    });
   }
 
   getMediasLinks();
@@ -72,9 +112,6 @@ function mediasFactory(data) {
 
   // A CE MOMENT, JE PENSE QUE JE PEUX GARDER CE QUE J'AI, IL SUFFIRAIT DE RECUPERER ENSUITE DANS L'URL LE TITRE DE L'IMAGE
   // UN PEU COMME ON L'A FAIT POUR AVOIR L'ID DU PHOTOGRAPHE, ET RESTITUER LA BONNE IMAGE DANS LA LIGHTBOX.... A SUIVRE
-
-  const lighbox = document.createElement('div');
-  lighbox.classList.add('lightbox');
 
   return {
     id,
